66fd2a8d67255afdd09e83d08b9e8b48a55fdc1a
CodeTestNedeljkovic <nikola.nedeljkovic@code.edu.rs>
2023-01-30 18:29:10 +0100

    Added testing code for XMLHttp

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// let xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+// 	if (this.readyState == 4 && this.status == 200) {
+// 		console.log(JSON.parse(xhttp.responseText));
+// 	}
+// };
+// xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
+// xhttp.send();
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     .then(json => json.json())
+//     .then(data => {
+//         console.log(data);
+//     });
